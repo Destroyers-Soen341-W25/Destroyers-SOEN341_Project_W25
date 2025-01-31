902bfba40476b3546ab9865bfd9249218cbb4dd1
George-Guirguis-1 <georgesamehpro32@gmail.com>
2025-01-31 15:58:07 -0500

    Update User.js

--- User.js
+++ User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     password: { type: String, required: true, unique: true },
 });
-
+//we need to find a way to include the chat history
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
