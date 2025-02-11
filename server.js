931a551d620e94eca1a24311ac9b9b1f35020b0c
mondaSam <mondasameh0@gmail.com>
2025-02-10 22:31:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 import createUser from './create_user.js';
 import getUser from './getuser.js';
 import db from './Database-conf.js';
+import removeChannel from './removechannel.js';
+import {assignUserToChannel,removeUserFromChannel} from './assign_user-channel.js';
+import {changeroles} from './changeroles.js';
 
 const app = express();
 app.use(bodyParser.json());
@@ -45,6 +48,47 @@ app.post('/login', async (req, res) => {
         console.log("Logged");
     } catch (error) {
         res.status(500).json({ message: 'Error logging in', error });
+    }
+});
+//Remove channel
+app.delete('/remove-channel',async (req,res) => {
+    const {channelId}=req.body;
+    try {
+        await removeChannel(channelId);
+        res.status(200).json({ message: 'Channel removed successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error removing channel', error });
+    }
+});
+//Change roles
+app.put('/change-role', async (req, res) => {
+    const { username, role } = req.body;
+    try {
+        const updatedUser = await changeroles(role, username);
+        res.status(200).json({ message: 'User role updated', updatedUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating role', error });
+    }
+});
+//Assign user to channel
+app.post('/assign-user',async(req,res)=>{
+    const{userId,channelId}=req.body;
+    try{
+        const updatedChannel= await assignUserToChannel(userId,channelId);
+        res.status(200).json({ message: 'User assigned to channel', updatedChannel });
+    }catch (error){
+        res.status(500).json({message:'Error assigning to channel',error});
+    }
+    
+});
+//Remove user from channel
+app.post('/remove-user', async (req, res) => {
+    const { userId, channelId } = req.body;
+    try {
+        const updatedChannel = await removeUserFromChannel(userId, channelId);
+        res.status(200).json({ message: 'User removed from channel', updatedChannel });
+    } catch (error) {
+        res.status(500).json({ message: 'Error removing user', error });
     }
 });
 
