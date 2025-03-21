6b8711737e37ff76144c69d66a786c665ac5dc36
alien-stephanie <voutur@hotmail.com>
2025-03-21 13:22:02 -0400

    Chat Ui

---
+++ destroyers_frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from "react-router-dom";
+import './App.css';
+import App from './App';
+import {Provider} from "./components/ui/provider";
+import {Toaster} from "./components/ui/toaster";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <BrowserRouter>
+          <Provider>
+              <Toaster />
+              <App />
+          </Provider>
+      </BrowserRouter>
+  </React.StrictMode>
+);
