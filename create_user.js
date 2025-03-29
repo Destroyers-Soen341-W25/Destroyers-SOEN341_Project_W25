8c85edc658a9e393da726427a5473ce56159548e
monda <mondasameh0@gmail.com>
2025-03-29 16:53:49 -0400

    new user function with status

--- create_user.js
+++ create_user.js
@@ -1,13 +1,18 @@
 import db from './Database-conf.js';
+import status from './setuserstatus.js';
 /*const user1 = {
-    "name":"monda",
-    "password":"monda2025",
-    "role":"admin",
+    "name":"i am a ghost",
+    "password":"54321",
+    "role":"member",
 }*/
 async function createUser(user) {
     try{
         const result = await db.collection('users').add(user);
         console.log('User added with ID: ', result.id);
+        await status(result.id,'Online');
+        console.log('User status set to Online');
+        return result.id;
+
     }catch(error){
         console.error('Error adding document: ', error);
     }
